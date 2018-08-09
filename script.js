f281514ae1c5ce50f7b9f7cda3278895e8ae66f1
Andrew Ingraham <aingraham@flatiron.com>
2018-08-09 00:39:51 -0400

    Adjust title and use local jquery

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const hired = Math.random() >= 0.5;
 
 let message;
+let title;
+
 if (hired) {
     message = 'YES!';
+    title = '🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉';
 } else {
     message = 'NO!';
+    title = "🔥You're fired. Go away.🔥";
 }
 
 $('#message').text(message);
+$('title').text(title);
