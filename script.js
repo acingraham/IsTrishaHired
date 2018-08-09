3e73306f1ac8b6b691c20f57e41c94509eb49edf
Andrew Ingraham <aingraham@flatiron.com>
2018-08-09 02:30:35 -0400

    Small change to title

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ if (hired) {
     src = 'https://www.rd.com/wp-content/uploads/2018/02/19_Adorable-Puppy-Pictures-that-Will-Make-You-Melt_391191067_chris_tina-760x506.jpg';
 } else {
     line1 = 'No.';
-    title = "🔥You're fired. Go away.🔥";
+    title = "🔥 You're fired 🔥 Go away 🔥";
     src = 'https://media.istockphoto.com/photos/cute-pug-with-sad-eyes-picture-id93276598?k=6&m=93276598&s=612x612&w=0&h=o_1ifcnV8iB-Ys-uJ13-YOjLCwEOTwsBvBtPzzwj_lA=';
 }
 
