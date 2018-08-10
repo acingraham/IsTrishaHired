18328023d290bb3cbed9a5eca97a12218fe86db5
Andrew Ingraham <aingraham@flatiron.com>
2018-08-10 15:36:00 -0400

    Finished

--- script.js
+++ script.js
@@ -4,17 +4,29 @@ let line1;
 let line2;
 let message;
 let title;
+let music;
 
 if (hired) {
     line1 = 'YES!!';
     title = '🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉';
-    src = 'https://www.rd.com/wp-content/uploads/2018/02/19_Adorable-Puppy-Pictures-that-Will-Make-You-Melt_391191067_chris_tina-760x506.jpg';
+    src = './img/yes' + (Math.floor(Math.random() * 2) + 1);
+    music = `
+        <audio id="yes-music" autoplay>
+          <source src="./music/DontSayGoodbye.mp3" type="audio/mp3">
+        </audio>
+    `;
 } else {
     line1 = 'No.';
     title = "🔥 You're fired 🔥 Go away 🔥";
-    src = 'https://media.istockphoto.com/photos/cute-pug-with-sad-eyes-picture-id93276598?k=6&m=93276598&s=612x612&w=0&h=o_1ifcnV8iB-Ys-uJ13-YOjLCwEOTwsBvBtPzzwj_lA=';
+    src = './img/no' + (Math.floor(Math.random() * 5) + 1);
+    music = `
+        <audio id="yes-music" autoplay>
+          <source src="./music/ThisIsWar5.mp3" type="audio/mp3">
+        </audio>
+    `;
 }
 
+$('body').append(music);
 $('#line1').text(line1);
 $('svg').toggle();
 $('title').text(title);
