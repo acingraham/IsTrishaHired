89a0e5bcaa69e7493a09797d57b040cc1c525655
Andrew Ingraham <aingraham@flatiron.com>
2019-04-27 02:13:36 -0400

    Add bday code

--- script.js
+++ script.js
@@ -1,36 +1,89 @@
-const hired = Math.random() >= 0.5;
-
 let line1;
-let line2;
-let message;
 let title;
 let music;
+const trishasBday = 'Apr 27';
+const todayIsTrishasBday = (new Date()).toDateString().indexOf(trishasBday) > -1
+                         || window.location.href.indexOf('bday') > -1;
 
-if (hired) {
-    line1 = 'YES!!';
-    title = '🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉';
-    src = './img/yes' + (Math.floor(Math.random() * 2) + 1);
-    music = `
-        <audio id="yes-music" autoplay>
-          <source src="./music/DontSayGoodbye.mp3" type="audio/mp3">
-        </audio>
-    `;
+let bdayMessages = [
+  {
+    message: 'Happy Birthday, Trisha!  Looking forward to seeing you back at Flatiron!',
+    author: 'Tom',
+  },
+  {
+    message: 'Hi, Trisha! Hope you have a lovely birthday :D Stay in Portugal! (jk see you soon!!!)',
+    author: 'Katie',
+  },
+  {
+    message: "Happy Birthday Trisha!  I know you'll blow us away at Flatiron just like you did at Escape the Room.",
+    author: 'Steve',
+  },
+  {
+    message: "Happy Birthday Teresa, I'm leaving Flatiron",
+    author: 'Thomas',
+  },
+  {
+    message: '404 - Well Wishes Not Found',
+    author: 'Andrew',
+  },
+];
+
+if (todayIsTrishasBday) {
+  bdayMessages = bdayMessages.map(o => `${o.message} <br>-${o.author}`);
+
+  // Trisha is misspelled intentionally.
+  title = '🎉 Happy Bday Tricia! 🎉';
+
+  $('#bday').css('display', 'flex');
+  $('title').text(title);
+
+  const options = {
+    strings: bdayMessages,
+    typeSpeed: 40,
+    shuffle: true,
+    loop: true,
+    fadeOut: true,
+    backDelay: 3000,
+  };
+
+  new Typed("#bdayMessage", options);
+
+  music = `
+    <audio autoplay>
+      <source src="./music/FirstKegStand.mp3" type="audio/mp3">
+    </audio>
+  `;
+
+  $('body').append(music);
 } else {
-    line1 = 'No.';
-    title = "🔥 You're fired 🔥 Go away 🔥";
-    src = './img/no' + (Math.floor(Math.random() * 5) + 1);
-    music = `
-        <audio id="yes-music" autoplay>
-          <source src="./music/ThisIsWar5.mp3" type="audio/mp3">
-        </audio>
-    `;
-}
+  const hired = Math.random() >= 0.5;
+  if (hired) {
+      line1 = 'YES!!';
+      title = '🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉';
+      src = './img/yes' + (Math.floor(Math.random() * 2) + 1);
+      music = `
+          <audio autoplay>
+            <source src="./music/DontSayGoodbye.mp3" type="audio/mp3">
+          </audio>
+      `;
+  } else {
+      line1 = 'No.';
+      title = "🔥 You're fired 🔥 Go away 🔥";
+      src = './img/no' + (Math.floor(Math.random() * 5) + 1);
+      music = `
+          <audio autoplay>
+            <source src="./music/ThisIsWar5.mp3" type="audio/mp3">
+          </audio>
+      `;
+  }
 
-$('body').append(music);
-$('#line1').text(line1);
-$('svg').toggle();
-$('title').text(title);
-$('img').attr('src', src);
-setTimeout(function() {
-  $('img').fadeIn();
-}, 6000);
+  $('#main').css('display', 'flex');
+  $('body').append(music);
+  $('#line1').text(line1);
+  $('svg').toggle();
+  $('title').text(title);
+  $('#proofImage').attr('src', src);
+  setTimeout(function() {
+    $('#proofImage').fadeIn();
+  }, 6000);
+}
